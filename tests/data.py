0f155d47bca4da69024c1975568b45454175c6dd
Maxim Voronov <voronovmv@gmail.com>
2026-01-31 17:18:02 +0300

    Исправлены замечания ревьюера по тестам

---
+++ tests/data.py
@@ -0,0 +1,20 @@
+# tests/data.py
+
+# Данные для параметризации названий книг
+VALID_BOOK_NAMES = ["Война и мир", "Преступление и наказание", "А" * 40]
+INVALID_BOOK_NAMES = ["", "А" * 41]
+
+# Названия книг
+BOOK_MAIN = "Война и мир"
+BOOK_FANTASY = "Мастер и Маргарита"
+BOOK_COMEDY = "Собачье сердце"
+BOOK_CHILD = "Конёк-Горбунок"
+BOOK_AGE_RATED = "Преступление и наказание"
+BOOK_UNKNOWN = "Неизвестная книга"
+
+# Жанры
+GENRE_FANTASY = "Фантастика"
+GENRE_COMEDY = "Комедии"
+GENRE_CARTOONS = "Мультфильмы"
+GENRE_DETECTIVES = "Детективы"
+INVALID_GENRE = "Роман"
